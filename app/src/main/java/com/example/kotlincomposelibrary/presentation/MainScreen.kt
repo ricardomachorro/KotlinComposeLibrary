b159b4cde53017ba7c8edea09818b9105c093bcf
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2025-05-08 19:59:52 -0600

    Coding of a Drawer

---
+++ app/src/main/java/com/example/kotlincomposelibrary/presentation/MainScreen.kt
@@ -0,0 +1,38 @@
+package com.example.kotlincomposelibrary.presentation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainScreen(){
+    val navController = rememberNavController()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text("Menu", modifier = Modifier.padding(16.dp))
+                NavigationDrawerItem(label = { Text("Settings") }, selected = false, onClick = { /* Navigate to Settings */ })
+                NavigationDrawerItem(label = { Text("About") }, selected = false, onClick = { /* Navigate to About */ })
+            }
+        }
+    ){
+
+    }
+
+
+
+
+}
